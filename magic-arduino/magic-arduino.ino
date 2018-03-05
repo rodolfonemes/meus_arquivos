de5c0df3b705bc7a72e3e3404fd41dfed358aaa6
rodolfonemes <31015589+rodolfonemes@users.noreply.github.com>
2018-03-04 23:13:12 -0300

    Add files via upload
    magic-arduino

---
+++ magic-arduino/magic-arduino.ino
@@ -0,0 +1,49 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial s(2,3);
+int placar1=20;
+int placar2=20;
+
+
+void setup() {
+  Serial.begin(9600);
+  s.begin(9600);
+  Serial.println("Inicio");
+}
+
+void loop() {
+
+  while(s.available()) {
+    char comando = s.read();
+    Serial.println(comando);
+    
+    switch (comando) {
+  case 'L':
+    placar1 = placar1 + 1;
+    Serial.print("Joagador 1 + ");
+    Serial.println(placar1);
+    break;
+  case 'R':
+   placar1 = placar1 - 1;
+    Serial.print("Joagador 1 - ");
+    Serial.println(placar1);
+    break;
+  
+  case 'F':
+    placar2 = placar2 + 1;
+    Serial.print("Joagador 2 + ");
+    Serial.println(placar2);
+    break;
+  
+  case 'B':
+   placar2 = placar2 - 1;
+    Serial.print("Joagador 2 - ");
+    Serial.println(placar2);
+  break;
+  
+  default:
+Serial.println(" ");
+    }
+  }
+
+}
